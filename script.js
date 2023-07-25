d819946445525591c37c9bf1115d36ea230ce1d8
David Ade <davemoneymachine@gmail.com>
2023-07-25 12:28:21 +0100

    [updated]: responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 "use strict";
 
-const sectionHero = document.querySelector(".section-hero");
+const navbar = document.querySelector(".navbar");
+
+const header = document.querySelector(".header");
+const navHeight = navbar.getBoundingClientRect().height;
+// console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) navbar.classList.add("fix");
+  else navbar.classList.remove("fix");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
